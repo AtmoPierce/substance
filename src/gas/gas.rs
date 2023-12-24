19f239bde044881d1d4f7f571570a6804c87ba22
Michael Angeles <michael.g.angeles21@gmail.com>
2023-12-23 22:09:14 -0800

    Updates

--- src/gas/gas.rs
+++ src/gas/gas.rs
@@ -1,3 +1,6 @@
 pub trait Gas{
-
+    fn reynolds_number_gas(&self, density: f64, flow_velocity: f64, characteristic_length: f64, dynamic_viscosity: f64)->f64{
+        let reynolds_number = density * flow_velocity * characteristic_length / dynamic_viscosity;
+        return reynolds_number;        
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -8,24 +8,33 @@ mod solid;
 use solid::solid::Solid;
 
 pub struct Air{
+    pressure: f64,
     density: f64,
-    isentropic_expansion_factor: f64
+    isentropic_expansion_factor: f64,
+    gas_constant: f64,
+    dynamic_viscosity: f64
 }
 
 impl Default for Air{
     fn default()->Self{
         Air{
-            density: 1.225,
-            isentropic_expansion_factor: 1.4
+            pressure: 101325.0,                             // Pa
+            density: 1.225,                                 // (kg/m^3)
+            isentropic_expansion_factor: 1.4,               // (dimensionless)
+            gas_constant: 287.057,                          // J/(kg*K)
+            dynamic_viscosity: 1.789 * 10.0_f64.powf(-5.0)  // (Pa*s)
         }
     }
 }
 
 impl Air{
-    pub fn new(density: f64, isentropic_expansion_factor: f64)->Self{
+    pub fn new(pressure: f64, density: f64, isentropic_expansion_factor: f64, gas_constant: f64, dynamic_viscosity: f64)->Self{
         Air{
+            pressure: pressure,
             density: density,
-            isentropic_expansion_factor: isentropic_expansion_factor
+            isentropic_expansion_factor: isentropic_expansion_factor,
+            gas_constant: gas_constant,
+            dynamic_viscosity: dynamic_viscosity
         }
     }
     pub fn get_density(&self)->f64{
@@ -33,8 +42,13 @@ impl Air{
     }
         
     pub fn characteristic_velocity(&self)->f64{
-        let characteristic_velocity = (self.isentropic_expansion_factor / self.density).powf(2.0);
+        let characteristic_velocity = (self.isentropic_expansion_factor * (self.pressure / self.density)).powf(0.5);
         return characteristic_velocity 
+    }
+
+    pub fn reynolds_number(&self, flow_velocity: f64, characteristic_length: f64)->f64{
+        let reynolds_number = self.reynolds_number_gas(self.density, flow_velocity, characteristic_length, self.dynamic_viscosity);
+        return reynolds_number;
     }
 }
 
